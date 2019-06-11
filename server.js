f0baa74c7fd2912b8a2d163ad747bc8bda9c4aa0
samlutz <sammlutz@gmail.com>
2019-06-11 14:13:35 -0400

    skeleton set up

---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+
+console.log("\n******************************************\n" +
+    "Grabbing every article headline, preview and link\n" +
+    "from the NYTimes website:" +
+    "\n******************************************\n");
+
+axios.get("https://www.nytimes.com").then(function (response) {
+
+    // Load the HTML into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    var $ = cheerio.load(response.data);
+
+    // An empty array to save the data that we'll scrape
+    var results = [];
+
+    // Select each element in the HTML body from which you want information.
+    // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+    // but be sure to visit the package's npm page to see how it works
+    $("article").each(function (i, element) {
+
+        var title = $(element).children().text();
+        var link = $(element).find("a").attr("href");
+        var preview = $(element).find("p").text();
+        // console.log($(element).find("p"));
+        // Save these results in an object that we'll push into the results array we defined earlier
+        results.push({
+            title: title,
+            link: link,
+            preview: preview
+        });
+    });
+
+    // Log the results once you've looped through each of the elements found with cheerio
+
+    console.log(results);
+});
