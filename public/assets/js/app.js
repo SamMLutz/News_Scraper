0d9a5c16f43ee32b477d5c33c64f5a001dbb7eb2
samlutz <sammlutz@gmail.com>
2019-06-17 15:19:48 -0400

    save article button and notes working correctly

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,7 +1,38 @@
+
 // // tester button click to check connection
-$(document).on("click", "#delete-article", function () {
+// $(document).on("click", "#delete-article", function () {
+//   // alert("correctly linked yo")
+//   console.log("correctly linked yoskiiii")
+//   var thisId = $(this).attr("data-id");
+//   // var thisId = "5d03dd00bcf25da1f292e791"
+//   // console.log("this = " + JSON.stringify($(this)))
+//   console.log("thisId = " + thisId)
+//     // Now make an ajax call for the Article
+//     $.ajax({
+//       method: "GET",
+//       url: "/articles/" + thisId
+//     })
+//       // With that done, add the note information to the page
+//       .then(function(data) {
+//         console.log("data: " + JSON.stringify(data));
+        
+//         $.ajax({
+//           method: "POST",
+//           url: "/savedArticles/" + thisId,
+//           data: {
+//             // title: data.title,
+//             saved: false
+//           }
+//         })
+//         // console.log("posted")
+//         location.reload();
+//       });
+// })
+
+$(document).on("click", "#save-article", function () {
   // alert("correctly linked yo")
-  console.log("correctly linked yoskiiii")
+  // console.log("saved button correctly linked yoskiiii")
+  // Save the id from the p tag
   var thisId = $(this).attr("data-id");
   // var thisId = "5d03dd00bcf25da1f292e791"
   // console.log("this = " + JSON.stringify($(this)))
@@ -17,46 +48,80 @@ $(document).on("click", "#delete-article", function () {
         
         $.ajax({
           method: "POST",
-          url: "/savedArticles/" + thisId,
-          data: {
-            // title: data.title,
-            saved: false
-          }
-        })
-        // console.log("posted")
-        location.reload();
-      });
-})
-
-$(document).on("click", "#save-article", function () {
-  // alert("correctly linked yo")
-  // console.log("saved button correctly linked yoskiiii")
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
-  // var thisId = "5d03dd00bcf25da1f292e791"
-  // console.log("this = " + JSON.stringify($(this)))
-  console.log("thisId = " + thisId)
-    // Now make an ajax call for the Article
-    $.ajax({
-      method: "GET",
-      url: "/savedArticles/" + thisId
-    })
-      // With that done, add the note information to the page
-      .then(function(data) {
-        console.log("data: " + JSON.stringify(data));
-        
-        $.ajax({
-          method: "POST",
           url: "/articles/" + thisId,
           data: {
             // title: data.title,
             saved: true
           }
-        })
+        }) 
         // console.log("posted")
         location.reload();
       });
 })
+
+$(document).on("click", "#article-notes", function() {
+  // Empty the notes from the note section
+  $("#notes").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function(data) {
+      console.log("data.note =" + data.note)
+      console.log("data: " + data);
+      // The title of the article
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#notes").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
+
+// When you click the savenote button
+$(document).on("click", "#savenote", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/savedArticles/" + thisId,
+    data: {
+      // Value taken from title input
+      title: $("#titleinput").val(),
+      // Value taken from note textarea
+      body: $("#bodyinput").val()
+    }
+  })
+    // With that done
+    .then(function(data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
+
 
 // $(document).on("click", "#save-article", function () {
 //   // alert("correctly linked yo")
@@ -102,27 +167,33 @@ $(document).on("click", "#save-article", function () {
 // $(document).on("click", "#clear", function () {
 //   // alert("correctly linked yo")
 //   // console.log("correctly linked yoskiiii")
-//   $.ajax({
-//     method: "GET",
-//     url: "/articles"
-//   }).then(function (data) {
-//     if (data) {
-//       db.Article.remove();
-//     }
-//   })
+//   $.getJSON("/articles", function (data) {
+//     // For each one
+//     // for (var i = 0; i < data.length; i++) {
+//     //   // Display the apropos information on the page
+//     //   //   $("#cardholder").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+//     //   //   $("#cardholder").append("<p> Title: " + data[i].title + "</p>")
+//     //   //   $("#cardholder").append("<div class='card'> <div class='card-header'>" + data[i].title + "</div> </div>")
+//     //   // $("#cardholder").append("<div class='card'> <div class='card-header'>" + data[i].title + "</div> <div class='card-body'> <h5 class='card-title'>" + data[i].preview + "</h5> <p class='card-text'>" + data[i].link + "</p> <a href='#' id='save-article' class='btn btn-primary' data-id=" + data[i].id + ">Save Article</a> </div> </div>")
+//     // }
+//     API.deleteArticles()
+//     console.log(data);
+//   });
+  
 // })
 
 // on scrape click
 $(document).on("click", "#scrape", function () {
   $.getJSON("/articles", function (data) {
     // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      //   $("#cardholder").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-      //   $("#cardholder").append("<p> Title: " + data[i].title + "</p>")
-      //   $("#cardholder").append("<div class='card'> <div class='card-header'>" + data[i].title + "</div> </div>")
-      // $("#cardholder").append("<div class='card'> <div class='card-header'>" + data[i].title + "</div> <div class='card-body'> <h5 class='card-title'>" + data[i].preview + "</h5> <p class='card-text'>" + data[i].link + "</p> <a href='#' id='save-article' class='btn btn-primary' data-id=" + data[i].id + ">Save Article</a> </div> </div>")
-    }
+    // for (var i = 0; i < data.length; i++) {
+    //   // Display the apropos information on the page
+    //   //   $("#cardholder").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    //   //   $("#cardholder").append("<p> Title: " + data[i].title + "</p>")
+    //   //   $("#cardholder").append("<div class='card'> <div class='card-header'>" + data[i].title + "</div> </div>")
+    //   // $("#cardholder").append("<div class='card'> <div class='card-header'>" + data[i].title + "</div> <div class='card-body'> <h5 class='card-title'>" + data[i].preview + "</h5> <p class='card-text'>" + data[i].link + "</p> <a href='#' id='save-article' class='btn btn-primary' data-id=" + data[i].id + ">Save Article</a> </div> </div>")
+    // }
+    
     console.log(data);
   });
   // alert("correctly linked yo")
--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get("/scrape", function (req, res) {
     });
 
     // Log the results once you've looped through each of the elements found with cheerio
-    res.send("Scrape Complete");
+    res.redirect("/");
     // console.log("result: " + JSON.stringify(result));
   });
 })
@@ -151,6 +151,7 @@ app.get("/savedArticles", function (req, res) {
 app.get("/articles/:id", function (req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Article.findOne({ _id: req.params.id })
+  .populate("note")
     // ..and populate all of the notes associated with it
     //   .populate("note")
     .then(function (dbArticle) {
@@ -178,25 +179,24 @@ app.get("/savedArticles/:id", function (req, res) {
     });
 });
 
-// Route for saving/updating saved Article's 
-// app.post("/articles/:id", function(req, res) {
-//   // Create a new note and pass the req.body to the entry
-//   db.Saved.create(req.body)
-//     .then(function(dbSaved) {
-//       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-//       return db.Saved.findOneAndUpdate({ _id: req.params.id }, { note: dbSaved._id }, { new: true });
-//     })
-//     .then(function(dbSaved) {
-//       // If we were able to successfully update an Article, send it back to the client
-//       res.json(dbSaved);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
+app.post("/savedArticles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 app.post("/articles/:id", function (req, res) {
   // Create a new note and pass the req.body to the entry
